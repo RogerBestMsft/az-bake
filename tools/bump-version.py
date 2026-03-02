b4f9c47bd8b5474f83b4ff4a40cfcfcf32f77e33
Roger Best <rbest@microsoft.com>
2026-03-02 14:08:55 -0800

    update versioning

--- tools/bump-version.py
+++ tools/bump-version.py
@@ -13,18 +13,31 @@ from packaging.version import parse as parse_version  # pylint: disable=unresolv
 parser = argparse.ArgumentParser()
 parser.add_argument('--major', action='store_true', help='bump major version')
 parser.add_argument('--minor', action='store_true', help='bump minor version')
+parser.add_argument('--patch', action='store_true', help='bump patch version (default when no bump flag is specified)')
+parser.add_argument('--pre', nargs='?', const='prerel', default=None, metavar='SUFFIX',
+                    help='add a prerelease suffix (default: dev). '
+                         'Matches preview-release workflow format: VERSION.SUFFIX<NUMBER>. '
+                         'Only updates setup.py. Can combine with --major/--minor.')
+parser.add_argument('--pre-number', type=int, default=0,
+                    help='prerelease number appended to the suffix (default: 0). '
+                         'In CI this maps to GITHUB_RUN_NUMBER.')
 parser.add_argument('--notes', nargs='*', default=['Bug fixes and minor improvements'], help='space seperated strings with release notes')
 
 args = parser.parse_args()
 
 major = args.major
 minor = args.minor
+patch = args.patch
+pre = args.pre
+pre_number = args.pre_number
 notes = '* {}'.format('\n* '.join(args.notes))
 
-if major and minor:
-    raise ValueError('usage error: --major | --minor')
+if sum([major, minor, patch]) > 1:
+    raise ValueError('usage error: --major | --minor | --patch')
 
-patch = not minor and not major
+# Default to patch bump when no bump flag is specified
+if not major and not minor:
+    patch = True
 
 version = None
 
@@ -45,87 +58,99 @@ if not version:
 
 version_old = parse_version(version)
 
-n_major = version_old.major + 1 if major else version_old.major
-n_minor = 0 if major else version_old.minor + 1 if minor else version_old.minor
-n_patch = 0 if major or minor else version_old.micro + 1
+# Compute the new version
+if pre is not None:
+    # Prerelease mode: matches preview-release.yml workflow format
+    # Bump base version (patch by default), then add prerelease suffix
+    n_major = version_old.major + 1 if major else version_old.major
+    n_minor = 0 if major else version_old.minor + 1 if minor else version_old.minor
+    n_patch = 0 if major or minor else version_old.micro + 1 if patch else version_old.micro
+    base = f'{n_major}.{n_minor}.{n_patch}'
 
-version_new = parse_version(f'{n_major}.{n_minor}.{n_patch}')
+    version_new_str = f'{base}.{pre}{pre_number}'
+    print(f'setting prerelease version: {version_old.public} -> {version_new_str}')
 
+    # Only update setup.py for prerelease versions (matches workflow behavior)
+    fmt_setup = 'VERSION = \'{}\''
 
-print(f'bumping version: {version_old.public} -> {version_new.public}')
+    print('..updating setup.py')
 
-fmt_setup = 'VERSION = \'{}\''
-fmt_readme = 'https://github.com/rogerbestmsft/az-bake/releases/latest/download/bake-{}-py3-none-any.whl'
-fmt_docker = 'https://github.com/rogerbestmsft/az-bake/releases/latest/download/bake-{}-py3-none-any.whl'
-# fmt_consts = 'https://github.com/rogerbestmsft/az-bake/releases/latest/download/bake-{}-py3-none-any.whl'
-fmt_history = '{}\n++++++\n{}\n\n{}'
+    with open(path_bake / 'setup.py', 'r') as f:
+        setup = f.read()
 
+    if fmt_setup.format(version) not in setup:
+        raise ValueError('version string not found in setup.py')
 
-print('..updating setup.py')
+    setup = setup.replace(fmt_setup.format(version), fmt_setup.format(version_new_str))
 
-with open(path_bake / 'setup.py', 'r') as f:
-    setup = f.read()
+    with open(path_bake / 'setup.py', 'w') as f:
+        f.write(setup)
 
-if fmt_setup.format(version_old.public) not in setup:
-    raise ValueError('version string not found in setup.py')
+    print(f'done: {version_new_str}')
 
-setup = setup.replace(fmt_setup.format(version_old.public), fmt_setup.format(version_new.public))
+else:
+    # Standard version bump (existing behavior)
+    n_major = version_old.major + 1 if major else version_old.major
+    n_minor = 0 if major else version_old.minor + 1 if minor else version_old.minor
+    n_patch = 0 if major or minor else version_old.micro + 1
 
-with open(path_bake / 'setup.py', 'w') as f:
-    f.write(setup)
+    version_new = parse_version(f'{n_major}.{n_minor}.{n_patch}')
 
+    print(f'bumping version: {version_old.public} -> {version_new.public}')
 
-print('..updating HISTORY.rst')
+    fmt_setup = 'VERSION = \'{}\''
+    fmt_readme = 'https://github.com/rogerbestmsft/az-bake/releases/latest/download/bake-{}-py3-none-any.whl'
+    fmt_docker = 'https://github.com/rogerbestmsft/az-bake/releases/latest/download/bake-{}-py3-none-any.whl'
+    fmt_history = '{}\n++++++\n{}\n\n{}'
 
-with open(path_bake / 'HISTORY.rst', 'r') as f:
-    history = f.read()
+    print('..updating setup.py')
 
-if version_old.public not in history:
-    raise ValueError('version string not found in HISTORY.rst')
+    with open(path_bake / 'setup.py', 'r') as f:
+        setup = f.read()
 
-history = history.replace(version_old.public, fmt_history.format(version_new.public, notes, version_old.public))
+    if fmt_setup.format(version_old.public) not in setup:
+        raise ValueError('version string not found in setup.py')
 
-with open(path_bake / 'HISTORY.rst', 'w') as f:
-    f.write(history)
+    setup = setup.replace(fmt_setup.format(version_old.public), fmt_setup.format(version_new.public))
 
+    with open(path_bake / 'setup.py', 'w') as f:
+        f.write(setup)
 
-print('..updating Dockerfile')
+    print('..updating HISTORY.rst')
 
-with open(path_builder / 'Dockerfile', 'r') as f:
-    docker = f.read()
+    with open(path_bake / 'HISTORY.rst', 'r') as f:
+        history = f.read()
 
-if fmt_docker.format(version_old.public) not in docker:
-    raise ValueError('version string not found in Dockerfile')
+    if version_old.public not in history:
+        raise ValueError('version string not found in HISTORY.rst')
 
-docker = docker.replace(fmt_docker.format(version_old.public), fmt_docker.format(version_new.public))
+    history = history.replace(version_old.public, fmt_history.format(version_new.public, notes, version_old.public))
 
-with open(path_builder / 'Dockerfile', 'w') as f:
-    f.write(docker)
+    with open(path_bake / 'HISTORY.rst', 'w') as f:
+        f.write(history)
 
+    print('..updating Dockerfile')
 
-print('..updating README.md')
+    with open(path_builder / 'Dockerfile', 'r') as f:
+        docker = f.read()
 
-with open(path_root / 'README.md', 'r') as f:
-    readme = f.read()
+    if fmt_docker.format(version_old.public) not in docker:
+        raise ValueError('version string not found in Dockerfile')
 
-if fmt_readme.format(version_old.public) not in readme:
-    raise ValueError('version string not found in README.md')
+    docker = docker.replace(fmt_docker.format(version_old.public), fmt_docker.format(version_new.public))
 
-readme = readme.replace(fmt_readme.format(version_old.public), fmt_readme.format(version_new.public))
+    with open(path_builder / 'Dockerfile', 'w') as f:
+        f.write(docker)
 
-with open(path_root / 'README.md', 'w') as f:
-    f.write(readme)
+    print('..updating README.md')
 
+    with open(path_root / 'README.md', 'r') as f:
+        readme = f.read()
 
-# print('..updating _constants.py')
+    if fmt_readme.format(version_old.public) not in readme:
+        raise ValueError('version string not found in README.md')
 
-# with open(path_bake / 'azext_bake' / '_constants.py', 'r') as f:
-#     constants = f.read()
+    readme = readme.replace(fmt_readme.format(version_old.public), fmt_readme.format(version_new.public))
 
-# if fmt_consts.format(version_old.public) not in constants:
-#     raise ValueError('version string not found in _constants.py')
-
-# constants = constants.replace(fmt_consts.format(version_old.public), fmt_consts.format(version_new.public))
-
-# with open(path_bake / 'azext_bake' / '_constants.py', 'w') as f:
-#     f.write(constants)
+    with open(path_root / 'README.md', 'w') as f:
+        f.write(readme)
